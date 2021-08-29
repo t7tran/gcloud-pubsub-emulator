50080a775e7c731e980f571ec1c3935775f97b6e
Tien Tran <tien@transfamily.info>
2021-08-29 11:18:47 +1000

    Support push subscriptions

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 set -e
 
-# create topics with two subscriptions each as soon as the emulator is up
-while ! nc -z localhost 8538; do sleep 0.5; done && \
-for topic in $TOPICS;do \
-  echo "Creating topic $topic"; \
-  curl -fsSLX PUT http://localhost:8538/v1/projects/$PROJECT_ID/topics/$topic; \
-  for sub in $SUB_NAMES;do \
-    curl -fsSLX PUT \
-      http://localhost:8538/v1/projects/$PROJECT_ID/subscriptions/${sub/TOPIC/$topic} \
-      -H 'Content-Type: application/json' \
-      -d "{\"topic\":\"projects/$PROJECT_ID/topics/$topic\",\"ackDeadlineSeconds\":$ACK_DEADLINE}"; \
-  done \
-done &
+bootstrap() {
+	while ! nc -z localhost ${PORT:-8538}; do sleep 0.5; done
+	for topic in $TOPICS;do
+		echo "Creating topic $topic"
+		curl -fsSLX PUT http://localhost:${PORT:-8538}/v1/projects/${PROJECT_ID:-project-id}/topics/$topic
+		for sub in $SUB_NAMES; do
+			PUSH_ENDPOINT=${sub##*=}
+			sub=${sub%%=*}
+			[[ "$PUSH_ENDPOINT" == "$sub" ]] && PUSH_ENDPOINT=
+			
+			curl -fsSLX PUT \
+			http://localhost:${PORT:-8538}/v1/projects/${PROJECT_ID:-project-id}/subscriptions/${sub/TOPIC/$topic} \
+			-H 'Content-Type: application/json' \
+			--data-binary @- <<-JSON
+				{
+					"topic": "projects/${PROJECT_ID:-project-id}/topics/$topic",
+					"ackDeadlineSeconds": ${ACK_DEADLINE:-10},
+					"pushConfig": {
+						"pushEndpoint": "${PUSH_ENDPOINT}"
+					}
+				}
+			JSON
+		done
+	done
+}
 
-exec "$@"
+# create topics with two subscriptions each as soon as the emulator is up
+bootstrap &
+
+if [[ -z "$@" ]]; then
+	[[ ! -d "${DATADIR:-/data}" ]] && mkdir -p "${DATADIR:-/data}"
+	exec gcloud beta emulators pubsub start --host-port=0.0.0.0:${PORT:-8538} --data-dir=${DATADIR:-/data} --project=${PROJECT_ID:-project-id}
+else
+	exec "$@"
+fi
